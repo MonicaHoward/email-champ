a64b2fc01ae55d4551c94a7f7597dc3f632de567
Monica Williams <jay.m.howard@gmail.com>
2021-05-03 17:44:24 -0500

    configured Google Strategy

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => { console.log(token, refreshToken, profile, done) })
+);
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'})
-})
+app.get(
+    '/auth/google', 
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT);
